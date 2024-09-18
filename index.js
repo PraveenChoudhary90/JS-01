e05aba44e82145f81eea9ea588fadbcaba7101c3
praveen choudhary <pc852180@gmail.com>
2024-09-18 12:33:35 +0530

    new data added

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+window.alert("praveen choudhary");
+
 // function
 function class3(){
     let a =parseInt( window.prompt("Enter number 1"));
@@ -5,3 +7,21 @@ function class3(){
     window.alert(a+b);
 }
 class3();
+
+
+function class4(){
+    // var variable
+    var a = "p35 aa";
+    console.log(a);
+    var a = 89;
+    console.log(a);
+    // let variable
+    let b = "p35 aa";
+    b = 89;
+    console.log(b)
+
+    // const variable
+    const c = "p35 aa";
+    console.log(c);
+}
+class4();
